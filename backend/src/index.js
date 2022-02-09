995da8f938937ce3db69af4c88fdc9678be58c51
Vinicius Carneiro Charleaux <vinicius.charleaux@gmail.com>
2022-02-09 19:49:45 -0300

    fix: cors implemented

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mysql = require('mysql2/promise');
+const cors = require('cors')
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 async function getConnection() {
     const connection = await mysql.createConnection({
