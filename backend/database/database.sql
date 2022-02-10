5079f7e5b92178dae8af6d1f7a9b32528c9303b8
Vinicius Carneiro Charleaux <vinicius.charleaux@gmail.com>
2022-02-10 18:21:20 -0300

    fix: correções e adição do meu sobre

--- backend/database/database.sql
+++ backend/database/database.sql
@@ -1,11 +1,8 @@
 create database transactions;
 use transactions;
 
-create table transactions (titulo varchar(150) not null, valor float not null, categoria varchar(150) not null, tipo int not null, datacao varchar(20) not null, id int NOT NULL AUTO_INCREMENT, PRIMARY KEY (id));
+create table transactions (titulo varchar(150) not null, valor double precision not null, categoria varchar(150) not null, tipo int not null, datacao varchar(20) not null, hora varchar(50) not null, id int NOT NULL AUTO_INCREMENT, PRIMARY KEY (id));
 create table contact (nome varchar(150) not null, email varchar(150) not null, message varchar(500), id int NOT NULL AUTO_INCREMENT, PRIMARY KEY (id));
 
- 
 select * from transactions;
 select * from contact;
-
-drop database teste;
